cf85c21fa12358a225f55d75281bcef2bde210a0
heshanu <heshanu97@gmail.com>
2022-05-28 23:57:24 +0530

    Added files

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  concat,
+  fromEvent,
+  interval,
+  noop,
+  observable,
+  Observable,
+  of,
+  timer,
+  merge,
+  Subject,
+  BehaviorSubject,
+  AsyncSubject,
+  ReplaySubject, from
+} from 'rxjs';
+import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
+
+
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  run() {
+
+
+
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,44 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatSliderModule } from '@angular/material/slider';
+import { AboutComponent } from './about/about.component';
+import { CommonComponent } from './common/common.component';
+import { CourseComponent } from './course/course.component';
+import { CourseDialogComponent } from './course-dialog/course-dialog.component';
+import { CourseCardListComponent } from './course-card-list/course-card-list.component';
+import { HomeComponent } from './home/home.component';
+import { LessonComponent } from './lesson/lesson.component';
+import { LoadingComponent } from './loading/loading.component';
+import { LoginComponent } from './login/login.component';
+import { MessagesComponent } from './messages/messages.component';
+import { RegisterComponent } from './register/register.component';
+import { SearchLessonsComponent } from './search-lessons/search-lessons.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    CommonComponent,
+    CourseComponent,
+    CourseDialogComponent,
+    CourseCardListComponent,
+    HomeComponent,
+    LessonComponent,
+    LoadingComponent,
+    LoginComponent,
+    MessagesComponent,
+    RegisterComponent,
+    SearchLessonsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
